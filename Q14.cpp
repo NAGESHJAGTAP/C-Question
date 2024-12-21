515bd3d49c200117adfc832ef2b5a790d04a9599
Nageshjagtap <nageshjagtap063@gmail.com>
2024-12-21 23:35:00 +0530

    solve the approach third

--- Q14.cpp
+++ Q14.cpp
@@ -1,29 +1,29 @@
 // 14. Add Element to the Beginning of an Array Example: Input: nums = [1, 2, 3, 4]; Output: nums = [0,1,2,3,4].
 
 // 1. Brute Force Approach (Fixed-Size Arrays)
-#include <iostream>
-using namespace std;
-void addElementToBeginning(int arr[], int size, int newElement) {
-    int updatedSize = size + 1;  
-    int updatedArr[updatedSize];  
+// #include <iostream>
+// using namespace std;
+// void addElementToBeginning(int arr[], int size, int newElement) {
+//     int updatedSize = size + 1;  
+//     int updatedArr[updatedSize];  
 
-    updatedArr[0] = newElement;
-    for (int i = 0; i < size; i++) {
-        updatedArr[i + 1] = arr[i];
-    }
-    cout << "Updated array: ";
-    for (int i = 0; i < updatedSize; i++) {
-        cout << updatedArr[i] << " ";
-    }
-    cout << endl;
-}
-int main() {
-    int nums[] = {1, 2, 3, 4}; 
-    int size = sizeof(nums) / sizeof(nums[0]);
-    int newElement = 0; 
-    addElementToBeginning(nums, size, newElement);
-    return 0;
-}
+//     updatedArr[0] = newElement;
+//     for (int i = 0; i < size; i++) {
+//         updatedArr[i + 1] = arr[i];
+//     }
+//     cout << "Updated array: ";
+//     for (int i = 0; i < updatedSize; i++) {
+//         cout << updatedArr[i] << " ";
+//     }
+//     cout << endl;
+// }
+// int main() {
+//     int nums[] = {1, 2, 3, 4}; 
+//     int size = sizeof(nums) / sizeof(nums[0]);
+//     int newElement = 0; 
+//     addElementToBeginning(nums, size, newElement);
+//     return 0;
+// }
 
 
 
@@ -41,27 +41,59 @@ int main() {
 
 
 // 2. Easy Approach (In-Place Shifting in Fixed-Size Array)
-#include <iostream>
-using namespace std;
-void addElementToBeginning(int arr[], int& size, int newElement) {
-    for (int i = size; i > 0; i--) {
-        arr[i] = arr[i - 1];
-    }
-    arr[0] = newElement;  
-    size++;  
-    cout << "Updated array: ";
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
+// #include <iostream>
+// using namespace std;
+// void addElementToBeginning(int arr[], int& size, int newElement) {
+//     for (int i = size; i > 0; i--) {
+//         arr[i] = arr[i - 1];
+//     }
+//     arr[0] = newElement;  
+//     size++;  
+//     cout << "Updated array: ";
+//     for (int i = 0; i < size; i++) {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+// }
 
-int main() {
-    int nums[10] = {1, 2, 3, 4};  
-    int size = 4;  
-    int newElement = 0;  
+// int main() {
+//     int nums[10] = {1, 2, 3, 4};  
+//     int size = 4;  
+//     int newElement = 0;  
 
-    addElementToBeginning(nums, size, newElement);
+//     addElementToBeginning(nums, size, newElement);
 
-    return 0;
-}
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// 3. Moderate Approach (Using Vectors)
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// void addElementToBeginning(vector<int>& nums, int newElement) {
+//     nums.insert(nums.begin(), newElement); 
+//     cout << "Updated array: ";
+//     for (int num : nums) {
+//         cout << num << " ";
+//     }
+//     cout << endl;
+// }   
+
+// int main() {
+//     vector<int> nums = {1, 2, 3, 4};
+//     int newElement = 0;  
+//     addElementToBeginning(nums, newElement);
+
+//     return 0;
+// }
