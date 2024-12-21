c1111882044fff77655fc05d73006f409dc5b1e8
Nageshjagtap <nageshjagtap063@gmail.com>
2024-12-21 23:41:34 +0530

    Q 15 solve the problem approch first

---
+++ Q15.cpp
@@ -0,0 +1,21 @@
+// 15. Remove the Last element Input: nums = [1, 2, 3, 4,5]; Output: nums = [1,2,3,4].
+
+// Approach 1: Brute Force (Create a New Array)
+#include <iostream>
+using namespace std;
+void removeLastElementBruteForce(int arr[], int size) {
+    int newArr[size - 1];  
+    for (int i = 0; i < size - 1; i++) {
+        newArr[i] = arr[i];
+    }
+    for (int i = 0; i < size - 1; i++) {
+        cout << newArr[i] << " ";
+    }
+    cout << endl;
+}
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    removeLastElementBruteForce(arr, size);  
+    return 0;
+}
