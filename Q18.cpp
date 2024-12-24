bb8388aad48a2d72218a4d78ea65e3a51eefb60e
Nageshjagtap <nageshjagtap063@gmail.com>
2024-12-25 00:23:34 +0530

    solve the approch fourth

--- Q18.cpp
+++ Q18.cpp
@@ -24,56 +24,92 @@
 // }
 
 
-// Approach 3: Recursive Solution
-
-#include <iostream>
-using namespace std;
-
-void printEvenIndexRecursive(int arr[], int size, int index = 0) {
-    if (index >= size) { 
-    return;
-    }
-    cout << arr[index];  
-    if (index + 2 < size) { 
-    cout << ", ";
-    }
-    printEvenIndexRecursive(arr, size, index + 2); 
-}
-int main() {
-    int arr[] = {10, 20, 30, 40, 50};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    cout << "[";
-    printEvenIndexRecursive(arr, size);
-    cout << "]" << endl;
-    return 0;
-}
-
-
-
-
-
-
-
 
 
 
 // Approach 2: Moderate (Skip Odd Indexes)
+// #include <iostream>
+// using namespace std;
+// void printEvenIndexModerate(int arr[], int size) {
+//     cout << "[";
+//     for (int i = 0; i < size; i += 2) { 
+//         cout << arr[i];                 
+//         if (i < size - 2) {             
+//             cout << ", ";
+//         }
+//     }
+//     cout << "]" << endl;
+// }
+
+// int main() {
+//     int arr[] = {10, 20, 30, 40, 50};
+//     int size = sizeof(arr) / sizeof(arr[0]);
+//     printEvenIndexModerate(arr, size);
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+// Approach 3: Recursive Solution
+
+// #include <iostream>
+// using namespace std;
+
+// void printEvenIndexRecursive(int arr[], int size, int index = 0) {
+//     if (index >= size) { 
+//     return;
+//     }
+//     cout << arr[index];  
+//     if (index + 2 < size) { 
+//     cout << ", ";
+//     }
+//     printEvenIndexRecursive(arr, size, index + 2); 
+// }
+// int main() {
+//     int arr[] = {10, 20, 30, 40, 50};
+//     int size = sizeof(arr) / sizeof(arr[0]);
+//     cout << "[";
+//     printEvenIndexRecursive(arr, size);
+//     cout << "]" << endl;
+//     return 0;
+// }
+
+
+
+
+
+
+
+// Approach 4: Using Temporary Array
 #include <iostream>
 using namespace std;
-void printEvenIndexModerate(int arr[], int size) {
-    cout << "[";
+
+void printEvenIndexWithTempArray(int arr[], int size) {
+    int temp[size];  
+    int tempIndex = 0;
+
     for (int i = 0; i < size; i += 2) { 
-        cout << arr[i];                 
-        if (i < size - 2) {             
+        temp[tempIndex++] = arr[i];  
+    }
+    cout << "[";
+    for (int i = 0; i < tempIndex; i++) {
+        cout << temp[i];
+        if (i < tempIndex - 1) { 
             cout << ", ";
         }
     }
     cout << "]" << endl;
 }
-
 int main() {
     int arr[] = {10, 20, 30, 40, 50};
     int size = sizeof(arr) / sizeof(arr[0]);
-    printEvenIndexModerate(arr, size);
+    printEvenIndexWithTempArray(arr, size);
     return 0;
 }
