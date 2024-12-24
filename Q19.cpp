1ec14c83d2de5c30fce86413888fca4cb66ccb3f
Nageshjagtap <nageshjagtap063@gmail.com>
2024-12-25 00:46:52 +0530

    solve the second approch

--- Q19.cpp
+++ Q19.cpp
@@ -3,24 +3,56 @@
 // 1. Brute Force Approach
 
 
+// #include <iostream>
+// using namespace std;
+// bool isSortedBruteForce(int arr[], int size) {
+// for (int i = 0; i < size - 1; i++) {  
+//  if (arr[i] > arr[i + 1]) {        
+//  return false;                  
+// }
+// }
+// return true; 
+// }
+// int main() {
+//     int arr[] = {1, 2, 3, 4, 5};
+//     int size = sizeof(arr) / sizeof(arr[0]);
+//     bool result = isSortedBruteForce(arr, size);
+//     if (result) {
+//    cout << "True" << endl;  
+//     } else {
+//            cout << "False" << endl;
+//     }
+//     return 0;
+// }
+
+
+
+
+
+
+// 2. Optimized Approach (Early Exit)
+
 #include <iostream>
 using namespace std;
-bool isSortedBruteForce(int arr[], int size) {
-for (int i = 0; i < size - 1; i++) {  
- if (arr[i] > arr[i + 1]) {        
- return false;                  
+
+bool isSortedOptimized(int arr[], int size) {
+ for (int i = 0; i < size - 1; i++) {
+ if (arr[i] > arr[i + 1]) {  
+  return false;          
 }
 }
-return true; 
+    return true;  
 }
 int main() {
-    int arr[] = {1, 2, 3, 4, 5};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    bool result = isSortedBruteForce(arr, size);
-    if (result) {
-   cout << "True" << endl;  
+ int arr[] = {1, 2, 3, 4, 5};
+ int size = sizeof(arr) / sizeof(arr[0]);
+
+ bool result = isSortedOptimized(arr, size);
+ if (result) {
+    cout << "True" << endl;  
     } else {
-           cout << "False" << endl;
+        cout << "False" << endl; 
     }
+
     return 0;
 }
