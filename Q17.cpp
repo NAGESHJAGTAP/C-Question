79fdcab598ca209a80755e51bfab68bf82d42fae
Nageshjagtap <nageshjagtap063@gmail.com>
2024-12-25 00:11:46 +0530

    solve the approch fourth

--- Q17.cpp
+++ Q17.cpp
@@ -34,100 +34,12 @@
 
 // Approach 2: Easy (Using Single Loop with Counters)
 
-#include <iostream>
-using namespace std;
-void countPositiveNegativeEasy(int arr[], int size) {
-    int positiveCount = 0, negativeCount = 0;
-    for (int i = 0; i < size; i++) {
-        (arr[i] > 0) ? positiveCount++ : negativeCount++; 
-    }
-    cout << "Positive count: " << positiveCount << endl;
-    cout << "Negative count: " << negativeCount << endl;
-}
-int main() {
-    int arr[] = {1, -2, 3, -4, 5, -6};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    countPositiveNegativeEasy(arr, size);
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-// Approach 3: Moderate (Separate Loops for Positive and Negative Counts)
-
-#include <iostream>
-using namespace std;
-void countPositiveNegativeModerate(int arr[], int size) {
-    int positiveCount = 0, negativeCount = 0;
-    for (int i = 0; i < size; i++) {
-  if (arr[i] > 0) {
-   positiveCount++;
-  }
-    }
-    for (int i = 0; i < size; i++) {
-   if (arr[i] < 0) {
-   negativeCount++;
-   }
-    }
-    cout << "Positive count: " << positiveCount << endl;
-    cout << "Negative count: " << negativeCount << endl;
-}
-int main() {
-    int arr[] = {1, -2, 3, -4, 5, -6};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    countPositiveNegativeModerate(arr, size);
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 17. Count how many positive and negative numbers are in arr = [1, -2, 3, -4, 5, -6] and print the result.
-
-// Approach 1: Brute Force
-
 // #include <iostream>
 // using namespace std;
-
-// void countPositiveNegativeBruteForce(int arr[], int size) {
-//     int positiveCount = 0; 
-//     int negativeCount = 0; 
-//     for (int i = 0; i < size; i++) { 
-//    if (arr[i] > 0) {          
-//    positiveCount++;
-//   } else if (arr[i] < 0) {   
-//    negativeCount++;
-//         }
+// void countPositiveNegativeEasy(int arr[], int size) {
+//     int positiveCount = 0, negativeCount = 0;
+//     for (int i = 0; i < size; i++) {
+//         (arr[i] > 0) ? positiveCount++ : negativeCount++; 
 //     }
 //     cout << "Positive count: " << positiveCount << endl;
 //     cout << "Negative count: " << negativeCount << endl;
@@ -135,7 +47,7 @@ int main() {
 // int main() {
 //     int arr[] = {1, -2, 3, -4, 5, -6};
 //     int size = sizeof(arr) / sizeof(arr[0]);
-//     countPositiveNegativeBruteForce(arr, size);
+//     countPositiveNegativeEasy(arr, size);
 //     return 0;
 // }
 
@@ -146,57 +58,80 @@ int main() {
 
 
 
-// Approach 2: Easy (Using Single Loop with Counters)
-
-#include <iostream>
-using namespace std;
-void countPositiveNegativeEasy(int arr[], int size) {
-    int positiveCount = 0, negativeCount = 0;
-    for (int i = 0; i < size; i++) {
-        (arr[i] > 0) ? positiveCount++ : negativeCount++; 
-    }
-    cout << "Positive count: " << positiveCount << endl;
-    cout << "Negative count: " << negativeCount << endl;
-}
-int main() {
-    int arr[] = {1, -2, 3, -4, 5, -6};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    countPositiveNegativeEasy(arr, size);
-    return 0;
-}
-
-
-
-
-
-
-
-
 
 
 
 // Approach 3: Moderate (Separate Loops for Positive and Negative Counts)
 
-#include <iostream>
-using namespace std;
-void countPositiveNegativeModerate(int arr[], int size) {
-    int positiveCount = 0, negativeCount = 0;
-    for (int i = 0; i < size; i++) {
-  if (arr[i] > 0) {
-   positiveCount++;
-  }
-    }
-    for (int i = 0; i < size; i++) {
-   if (arr[i] < 0) {
-   negativeCount++;
-   }
-    }
-    cout << "Positive count: " << positiveCount << endl;
-    cout << "Negative count: " << negativeCount << endl;
-}
-int main() {
-    int arr[] = {1, -2, 3, -4, 5, -6};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    countPositiveNegativeModerate(arr, size);
-    return 0;
-}
+// #include <iostream>
+// using namespace std;
+// void countPositiveNegativeModerate(int arr[], int size) {
+//     int positiveCount = 0, negativeCount = 0;
+//     for (int i = 0; i < size; i++) {
+//   if (arr[i] > 0) {
+//    positiveCount++;
+//   }
+//     }
+//     for (int i = 0; i < size; i++) {
+//    if (arr[i] < 0) {
+//    negativeCount++;
+//    }
+//     }
+//     cout << "Positive count: " << positiveCount << endl;
+//     cout << "Negative count: " << negativeCount << endl;
+// }
+// int main() {
+//     int arr[] = {1, -2, 3, -4, 5, -6};
+//     int size = sizeof(arr) / sizeof(arr[0]);
+//     countPositiveNegativeModerate(arr, size);
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+// Approach 4: Optimal Approach (Short-Circuit Evaluation)
+
+// #include <iostream>
+// using namespace std;
+
+// void countPositiveNegativeOptimal(int arr[], int size) {
+//     int positiveCount = 0, negativeCount = 0;
+
+//   for (int i = 0; i < size; i++) {
+//    if (arr[i] > 0) {
+//   positiveCount++;  // Increment positive counter
+//   } else {
+//   negativeCount++;  // Increment negative counter
+//    }
+//     }
+
+//   cout << "Positive count: " << positiveCount << endl;
+//   cout << "Negative count: " << negativeCount << endl;
+// }
+
+// int main() {
+//     int arr[] = {1, -2, 3, -4, 5, -6};
+//     int size = sizeof(arr) / sizeof(arr[0]);
+
+//     countPositiveNegativeOptimal(arr, size);
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
