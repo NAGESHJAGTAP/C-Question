6720fa387b87eda2f8bc4f136e721fb8007f6b1a
Nageshjagtap <nageshjagtap063@gmail.com>
2024-12-26 23:50:25 +0530

    Q22 solve the problem approch first

---
+++ Q22.cpp
@@ -0,0 +1,21 @@
+// 22. Write a program to find the length of a given string.Example:Input:inputString = "Hello, World!"; Output:13;
+
+// Approach 1: Brute Force (Manual Count)
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int findlength(string str) { 
+    int count = 0;
+    for (int i = 0; str[i] != '\0'; i++) { 
+        count++;
+    }
+    return count;
+}
+
+int main() {
+    string input = "hello world!";
+    cout << findlength(input) << endl; 
+    return 0;
+}
