d2e9227c8c250ea22a099a6ae3e5917c2b500925
Nageshjagtap <nageshjagtap063@gmail.com>
2024-12-27 00:47:11 +0530

    solve the approch third

--- Q23.cpp
+++ Q23.cpp
@@ -17,14 +17,30 @@
 
 
 // Approach 2: Using + Operator (Easy)
+// #include <iostream>
+// using namespace std;
+// string concat(string str1, string str2) {
+//     return str1 + str2; 
+// }
+// int main() {
+//     string str1 = "hello";
+//     string str2 = " world";
+//     cout  << concat(str1, str2) << endl;
+//     return 0;
+// }
+
+
+
+// Approach 3: Using append() Method (Moderate)
 #include <iostream>
 using namespace std;
-string concat(string str1, string str2) {
-    return str1 + str2; 
+string concatenateWithAppend(string str1, string str2) {
+    str1.append(str2); 
+    return str1; 
 }
 int main() {
     string str1 = "hello";
     string str2 = " world";
-    cout  << concat(str1, str2) << endl;
+    cout << "Concatenated String (Using append()): " << concatenateWithAppend(str1, str2) << endl;
     return 0;
 }
