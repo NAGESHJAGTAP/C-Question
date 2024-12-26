faae610c703ef7c41507a7cba37098c454208207
Nageshjagtap <nageshjagtap063@gmail.com>
2024-12-26 08:45:47 +0530

    solve the third approch

--- Q20.cpp
+++ Q20.cpp
@@ -31,25 +31,58 @@
 
 
 // 2. Moderate Approach (Single Loop)
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int findDifferenceSingleLoop(int arr[], int size) {
-    int maxElement = arr[0];
-    int minElement = arr[0];
-    for (int i = 1; i < size; i++) {
-        if (arr[i] > maxElement) {
-            maxElement = arr[i];
-        }
-        if (arr[i] < minElement) { 
-            minElement = arr[i];
-        }
-    }
+// int findDifferenceSingleLoop(int arr[], int size) {
+//     int maxElement = arr[0];
+//     int minElement = arr[0];
+//     for (int i = 1; i < size; i++) {
+//         if (arr[i] > maxElement) {
+//             maxElement = arr[i];
+//         }
+//         if (arr[i] < minElement) { 
+//             minElement = arr[i];
+//         }
+//     }
+//     return maxElement - minElement;
+// }
+// int main() {
+//     int arr[] = {80, 30, 70, 50, 20};
+//     int size = sizeof(arr) / sizeof(arr[0]); 
+//     cout << "Difference (Single Loop): " << findDifferenceSingleLoop(arr, size) << endl;
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3. Optimal Approach (Built-in Functions)
+
+#include <iostream>
+#include <algorithm> 
+using namespace std;
+int findDifferenceBuiltIn(int arr[], int size) {
+    int maxElement = *max_element(arr, arr + size); 
+    int minElement = *min_element(arr, arr + size);
     return maxElement - minElement;
 }
+
 int main() {
     int arr[] = {80, 30, 70, 50, 20};
     int size = sizeof(arr) / sizeof(arr[0]); 
-    cout << "Difference (Single Loop): " << findDifferenceSingleLoop(arr, size) << endl;
+    cout << "Difference (Built-in): " << findDifferenceBuiltIn(arr, size) << endl;
     return 0;
 }
