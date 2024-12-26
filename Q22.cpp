4acfcbad96ced3c7a8a4cd5b59237b8287d5b045
Nageshjagtap <nageshjagtap063@gmail.com>
2024-12-27 00:02:42 +0530

    solve the approch second

--- Q22.cpp
+++ Q22.cpp
@@ -2,20 +2,36 @@
 
 // Approach 1: Brute Force (Manual Count)
 
-#include <iostream>
-#include <string>
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// int findlength(string str) { 
+//     int count = 0;
+//     for (int i = 0; str[i] != '\0'; i++) { 
+//         count++;
+//     }
+//     return count;
+// }
+
+// int main() {
+//     string input = "hello world!";
+//     cout << findlength(input) << endl; 
+//     return 0;
+// }
+
+
+
+// Approach 2: Using size() or length() Method (Easy)
+
+# include <iostream>
+# include <string>
 using namespace std;
-
-int findlength(string str) { 
-    int count = 0;
-    for (int i = 0; str[i] != '\0'; i++) { 
-        count++;
-    }
-    return count;
+int findlength(string str){
+  return str.length();
 }
-
-int main() {
-    string input = "hello world!";
-    cout << findlength(input) << endl; 
+int main(){
+    string input="hello world!";
+    cout << findlength(input) << endl;
     return 0;
 }
