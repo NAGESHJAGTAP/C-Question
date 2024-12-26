a63f0b3b9e119ed11acf2e3aff299e6bb12f3e78
Nageshjagtap <nageshjagtap063@gmail.com>
2024-12-27 00:45:41 +0530

    solve the approch second

--- Q23.cpp
+++ Q23.cpp
@@ -1,16 +1,30 @@
 // 23. Write a program to concatenate two given strings. Example: Input: ("hello", " world"), Output: "hello world".
 // Approach 1: Brute Force (Manual Concatenation)
+// #include <iostream>
+// using namespace std;
+// string concatenateManual(string str1, string str2) {
+//     for (int i = 0; i < str2.length(); i++) { 
+//         str1 += str2[i]; 
+//     }
+//     return str1; 
+// }
+// int main() {
+//     string str1 = "hello";  
+//     string str2 = " world"; 
+//     cout << "Concatenated String (Brute Force): " << concatenateManual(str1, str2) << endl;
+//     return 0;
+// }
+
+
+// Approach 2: Using + Operator (Easy)
 #include <iostream>
 using namespace std;
-string concatenateManual(string str1, string str2) {
-    for (int i = 0; i < str2.length(); i++) { 
-        str1 += str2[i]; 
-    }
-    return str1; 
+string concat(string str1, string str2) {
+    return str1 + str2; 
 }
 int main() {
-    string str1 = "hello";  
-    string str2 = " world"; 
-    cout << "Concatenated String (Brute Force): " << concatenateManual(str1, str2) << endl;
+    string str1 = "hello";
+    string str2 = " world";
+    cout  << concat(str1, str2) << endl;
     return 0;
 }
